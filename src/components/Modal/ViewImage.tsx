579c9a75fbf5be0e36d42720cbff5e9d9138b4fd
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2023-04-01 16:12:07 -0300

    done

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,29 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent
+        mx="auto"
+        w="auto"
+        h="auto"
+        maxW={900}
+        maxH={600}
+      >
+        <ModalBody p={0}>
+          <Image src={imgUrl} maxW={900} maxH={600} />
+        </ModalBody>
+        <ModalFooter
+          bg="pGray.800"
+          h="2rem"
+          borderBottomRadius="md"
+        >
+          <Link href={imgUrl} target="_blank">
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,18 +18,34 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+    async ({ pageParam = null }) => {
+      const response = await api.get('api/images', {
+        params: {
+          after: pageParam,
+        }
+      })
+
+      return response.data;
+    },
+    {
+      getNextPageParam: ({after}) => {
+        return after ? after : null;
+      },
+    }
   );
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    console.log(data)
+    return data?.pages.flatMap(page => page.data.flat());
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if(isLoading){
+    return <Loading />
+  }
 
-  // TODO RENDER ERROR SCREEN
+  if(isError) {
+    return <Error />
+  }
 
   return (
     <>
@@ -37,7 +53,15 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button
+            mt="10"
+            onClick={() => fetchNextPage()}
+            disabled={isFetchingNextPage}
+          >
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
